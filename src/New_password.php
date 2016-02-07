914fe8de15dc892f1c2a2c69683c87bd31df118c
Salim <salim_assad@hotmail.com>
2016-02-07 15:01:41 -0600

    Se añadió el uso de bootstrap para que no se vea tan p****o.

--- src/New_password.php
+++ src/New_password.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET['idCliente'])) {
+if (isset($_GET['idCliente'])) {
     $idCliente = $_GET['idCliente'];
     ?>
     <!DOCTYPE html>
@@ -7,24 +7,30 @@ if(isset($_GET['idCliente'])) {
     <head lang="es">
         <meta charset="UTF-8">
         <title>Nueva contraseña</title>
+
+        <link href="css/bootstrap.css" rel="stylesheet">
     </head>
     <body>
-    <div>
-        <form action="Reset.php" method="post">
-            <input type="hidden" name="idCliente" value="<?php echo $idCliente ?>">
-            <label>New password</label>
-            <br>
-            <input type="password" name="password">
-            <label>Verify new password</label>
-            <br>
-            <input type="password" name="verify_password">
-            <br>
-            <input type="submit">
-        </form>
+    <div align="center">
+        <div align="left" style="width: 230px; height: 140px; padding: 10px; margin-top: 130px;">
+            <form action="Reset.php" method="post">
+                <input type="hidden" name="idCliente" value="<?php echo $idCliente ?>">
+                <label for="password">Nueva contraseña</label>
+                <br>
+                <input class="form-control" type="password" id="password" name="password">
+                <label for="verify_password">Verificar nueva contraseña</label>
+                <br>
+                <input class="form-control" type="password" id="verify_password" name="verify_password">
+
+                <div align="center" style="margin: 10px">
+                    <input class="btn" type="submit">
+                </div>
+            </form>
+        </div>
     </div>
     </body>
     </html>
 <?php
-}else{
+} else {
     header("Location: Login.php");
 }
