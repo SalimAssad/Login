3290764efc838440dda54092fad4913fc459ca9a
Salim <salim_assad@hotmail.com>
2016-02-08 00:21:52 -0600

    Jaime tiene mala suerte, añado su login por el.

---
+++ src/Login.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jaime
+ * Date: 2/7/2016
+ * Time: 3:14 PM
+ */
+include_once("MySQLConnection.php");
+?>
+<!DOCTYPE html>
+<head lang="en">
+    <meta charset="UTF-8">
+</head>
+<body bgcolor="#EEEEEE">
+<div align="center">
+    <div style="width: 200px; height: 150px; padding: 20px; background-color: rgba(0, 0, 0, 0); border: double">
+        <form action="login_verify.php" method="post">
+            <label>Usuario</label>
+            <br>
+            <input class="form-control" type="text" name="user" required>
+            <br>
+            <label>Contraseña</label>
+            <br>
+            <input class="form-control" type="password" name="password" required>
+            <br>
+            <br>
+            <input type="submit">
+            <a href="Registro.php"> <input type="button" value="Register"> </a>
+            <br>
+            <a href="Send_reset_email.php" <label>Olvidaste tu contraseña? Haz click aquí</label> </a>
+
+            
+            
+
+        </form>
+    </div>
+</div>
+</body>
---
+++ src/Login_verify.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jaime
+ * Date: 2/7/2016
+ * Time: 3:14 PM
+ */
+include_once("MySQLConnection.php");
+
+$user = $_POST["user"];
+$password = $_POST["password"];
+
+
+$query = mysqli_query($connect, "select * from clientes where idCliente = $user");
+
+if(mysqli_num_rows($query) > 0){
+    $row = mysqli_fetch_assoc($query);
+
+    if($row['mostrar']) {
+        if (md5($password) == $row['password']) {
+            session_start();
+            $_SESSION['name'] = $row['nombre'];
+            header('Location: Menu.php');
+        } else {
+            header('Location: Login.php');
+        }
+    }else{
+        header('Location: Login.php');
+    }
+}else{
+    $validation_query = mysqli_query($connect, "select idCliente from clientes");
+    if(mysqli_num_rows($validation_query) == 0){
+        header('Location: Registro.php');
+    }else{
+        header('Location: Login.php');
+    }
+}
+
+mysqli_close($connect);
+?>
